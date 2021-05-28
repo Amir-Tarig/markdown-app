9d2ec91bfc19931ebadb02c9eb58f9fdafba427a
Miro <MiroPhoenix91@gmail.com>
2021-05-29 00:01:29 +0200

    markdown app

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Markdown />
+</template>
+
+<script>
+import Markdown from './components/Markdown.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Markdown
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Markdown.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <article>left</article>
+    <article>right</article>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
